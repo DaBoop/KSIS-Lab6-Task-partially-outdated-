cd85c54c3b69bea31e0d80618eedf6ee6ca86634
DaBoop <arti_2002_2011@mail.ru>
2020-11-14 12:25:45 +0300

    maskcheck fix

--- Source.cpp
+++ Source.cpp
@@ -42,7 +42,7 @@ bool OctetCheck(vector<string> str) // returns false if one of the octets or the
 
 bool MaskCheck(string str) // returns false if mask sequence is wrong
 {
-	if (str[0] != 1)
+	if (str[0] != '1')
 		return false;
 	bool zeroMet = false;
 	for (char letter : str)
