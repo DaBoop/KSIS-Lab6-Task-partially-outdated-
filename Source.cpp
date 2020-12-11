8b3c6b8abd40910aaa02252e3847ad46dbcdd9ad
DaBoop <arti_2002_2011@mail.ru>
2020-12-12 00:52:27 +0300

    SuperUpdate

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
@@ -63,92 +64,202 @@ bool MaskCheck(string str) // returns false if mask sequence is wrong
 void main()
 {
 	string ip, mask;
-	cout << "IP:\t";
-	cin >> ip;
-	cout << "Mask:\t";
-	cin >> mask;
-
-	vector <string> ipTrim = Trim(ip);
-	vector <string> maskTrim = Trim(mask);
-
-	if (!OctetCheck(ipTrim))
-	{
-		cout << "Wrong ip";
-		return;
-	}
-	if (!OctetCheck(maskTrim))
-	{
-		cout << "Wrong mask";
-		return;
-	}
+	
 
 	int temp;
 	char tempstr[30];
 	string str;
 	string binaryip = "";
 	string binarymask = "";
-
-	
-	for (int i : {0, 1, 2, 3})
+	vector <string> ipTrim;
+	vector <string> maskTrim;
+	int option = 0;
+	cout << "Choose option:\n1 - Console input\n2 - File input(input.txt)\n";
+	cin >> option;
+	if (option == 1)
+		while (true)
 	{
-		temp = stoi(ipTrim[i]);
-		_itoa_s(temp, tempstr, 2);
-		str = tempstr; // for length check only
-		for (int i = str.length(); i < 8; i++)
-			binaryip += '0';
-		binaryip += tempstr;
-	}
+		binaryip = "";
+		binarymask = "";
 
-	for (int i : {0, 1, 2, 3})
+		cin.clear();
+		cout << "IP:\t";
+		cin >> ip;
+		cout << "Mask:\t";
+		cin >> mask;
+
+		ipTrim = Trim(ip);
+		maskTrim = Trim(mask);
+
+		if (!OctetCheck(ipTrim))
+		{
+			cout << "Wrong ip\n\n";
+			continue;
+		}
+		if (!OctetCheck(maskTrim))
+		{
+			cout << "Wrong mask\n\n";
+			continue;
+		}
+		for (int i : {0, 1, 2, 3})
+		{
+			temp = stoi(ipTrim[i]);
+			_itoa_s(temp, tempstr, 2);
+			str = tempstr; // for length check only
+			for (int i = str.length(); i < 8; i++)
+				binaryip += '0';
+			binaryip += tempstr;
+		}
+
+		for (int i : {0, 1, 2, 3})
+		{
+			temp = stoi(maskTrim[i]);
+			_itoa_s(temp, tempstr, 2);
+			str = tempstr; // for length check only
+			for (int i = str.length(); i < 8; i++)
+				binarymask += '0';
+			binarymask += tempstr;
+		}
+
+		if (!MaskCheck(binarymask))
+		{
+			cout << "Wrong mask sequence\n\n";
+			continue;
+		}
+
+		cout << "IP:\t";
+		for (int i = 0; i < 32; i++)
+		{
+			cout << binaryip[i];
+			if (i % 8 == 7 && i != 31)
+				cout << ".";
+		}
+		cout << endl << "Mask:\t";
+		for (int i = 0; i < 32; i++)
+		{
+			cout << binarymask[i];
+			if (i % 8 == 7 && i != 31)
+				cout << ".";
+		}
+
+		cout << endl << "Subnet:\t";
+		for (int i : {0, 1, 2, 3})
+		{
+			cout << (atoi(ipTrim[i].c_str()) & atoi(maskTrim[i].c_str()));
+			if (i != 3)
+				cout << ".";
+		}
+		cout << endl << "Host:\t";
+		for (int i : {0, 1, 2, 3})
+		{
+			cout << (atoi(ipTrim[i].c_str()) & ~atoi(maskTrim[i].c_str()));
+			if (i != 3)
+				cout << ".";
+		}
+
+
+
+		cout << endl << endl;
+
+	}
+	else
 	{
-		temp = stoi(maskTrim[i]);
-		_itoa_s(temp, tempstr, 2);
-		str = tempstr; // for length check only
-		for (int i = str.length(); i < 8; i++)
-			binarymask += '0';
-		binarymask += tempstr;
+		char* line = new char[100];
+		
+		ifstream fin("input.txt");
+		while (!fin.eof())
+		{
+			fin.getline(line, 100);
+			if (line == "") continue;
+			ip = line;
+			fin.getline(line, 100);
+			if (line == "") continue;
+			mask = line;
+
+			binaryip = "";
+			binarymask = "";
+
+			cin.clear();
+			cout << "IP:\t" << ip << "\n";
+			cout << "Mask:\t" << mask << "\n";
+			ipTrim = Trim(ip);
+			maskTrim = Trim(mask);
+
+			if (!OctetCheck(ipTrim))
+			{
+				cout << "Wrong ip\n\n";
+				continue;
+			}
+			if (!OctetCheck(maskTrim))
+			{
+				cout << "Wrong mask\n\n";
+				continue;
+			}
+			for (int i : {0, 1, 2, 3})
+			{
+				temp = stoi(ipTrim[i]);
+				_itoa_s(temp, tempstr, 2);
+				str = tempstr; // for length check only
+				for (int i = str.length(); i < 8; i++)
+					binaryip += '0';
+				binaryip += tempstr;
+			}
+
+			for (int i : {0, 1, 2, 3})
+			{
+				temp = stoi(maskTrim[i]);
+				_itoa_s(temp, tempstr, 2);
+				str = tempstr; // for length check only
+				for (int i = str.length(); i < 8; i++)
+					binarymask += '0';
+				binarymask += tempstr;
+			}
+
+			if (!MaskCheck(binarymask))
+			{
+				cout << "Wrong mask sequence\n\n";
+				continue;
+			}
+
+			cout << "IP:\t";
+			for (int i = 0; i < 32; i++)
+			{
+				cout << binaryip[i];
+				if (i % 8 == 7 && i != 31)
+					cout << ".";
+			}
+			cout << endl << "Mask:\t";
+			for (int i = 0; i < 32; i++)
+			{
+				cout << binarymask[i];
+				if (i % 8 == 7 && i != 31)
+					cout << ".";
+			}
+
+			cout << endl << "Subnet:\t";
+			for (int i : {0, 1, 2, 3})
+			{
+				cout << (atoi(ipTrim[i].c_str()) & atoi(maskTrim[i].c_str()));
+				if (i != 3)
+					cout << ".";
+			}
+			cout << endl << "Host:\t";
+			for (int i : {0, 1, 2, 3})
+			{
+				cout << (atoi(ipTrim[i].c_str()) & ~atoi(maskTrim[i].c_str()));
+				if (i != 3)
+					cout << ".";
+			}
+
+			
+
+			cout << endl << endl;
+		}
+		fin.close();
+		cout << "Work finished\n";
+		
 	}
 
-	if (!MaskCheck(binarymask))
-	{
-		cout << "Wrong mask sequence";
-		return;
-	}
-	
-	cout << "IP:\t";
-	for (int i = 0; i < 32; i++)
-	{
-		cout << binaryip[i];
-		if (i % 8 == 7 && i != 31)
-			cout << ".";
-	}
-	cout << endl << "Mask:\t";
-	for (int i = 0; i < 32; i++)
-	{
-		cout << binarymask[i];
-		if (i % 8 == 7 && i != 31)
-			cout << ".";
-	}
-
-	cout << endl << "Subnet:\t";
-	for (int i : {0, 1, 2, 3})
-	{ 
-		cout << (atoi(ipTrim[i].c_str()) & atoi(maskTrim[i].c_str()));
-		if (i != 3)
-			cout << ".";
-	}
-	cout << endl << "Host:\t";
-	for (int i : {0, 1, 2, 3})
-	{
-		cout << (atoi(ipTrim[i].c_str()) & ~atoi(maskTrim[i].c_str()));
-		if (i != 3)
-			cout << ".";
-	}
-	
-	
-
-	cout << endl;
-
-
-
+	getchar();
+	getchar();
 }
